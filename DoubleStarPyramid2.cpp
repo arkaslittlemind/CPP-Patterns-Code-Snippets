029ad838586bcc594224e0b040b71ee876ec9b67
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-10-20 13:11:17 +0530

    Create DoubleStarPyramid2.cpp

---
+++ DoubleStarPyramid2.cpp
@@ -0,0 +1,34 @@
+//Write a programme to print the following pattern//
+
+//Pattern//
+
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+ * * * *
+  * * *
+   * *
+    *
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter Size:";
+    cin>>n;
+    for (int i = 0; i < 2*n; i++)
+    {
+        int totalColsinRow = i > n ? 2 * n - i: i;
+        for (int j = 0; j < totalColsinRow; j++)
+        {
+        cout<<"* ";
+        }
+        cout<<"\n";
+        
+    }
+    return 0;
+}
