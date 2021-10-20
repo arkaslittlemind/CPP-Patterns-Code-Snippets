8ee2d99da5d1c16e50f655a894e7af5d540e5099
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-10-20 12:06:04 +0530

    Create RectangleStarPattern.cpp

---
+++ RectangleStarPattern.cpp
@@ -0,0 +1,28 @@
+//Write a programme to print the following pattern//
+
+//Pattern//
+
+* * * * * 
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter Size:";
+    cin>>n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout<<"* ";
+        }
+        cout<<"\n";
+        
+    }
+    return 0;
+}
